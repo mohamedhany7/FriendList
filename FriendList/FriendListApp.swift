d07b14546e5cbe2f7e72a298533fb6de65f1f573
mohamedhany7 <mohameddhanyy7@gmail.com>
2022-10-13 18:27:32 +0200

    Initial Commit

---
+++ FriendList/FriendListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendListApp.swift
+//  FriendList
+//
+//  Created by Mohamed Hany on 13/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FriendListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
